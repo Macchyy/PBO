7ee20b779010485a938960e44a2417b2f2864dc7
RaditiaAstarina <148021980+RaditiaAstarina@users.noreply.github.com>
2023-10-19 21:25:12 +0800

    Update JB1_F1B019115.java

--- JB1_F1B019115.java
+++ JB1_F1B019115.java
@@ -4,6 +4,8 @@ class Pelapor {
  String nama;
  String nik;
  String laporan;
+ String nohp;
+ String umur;
 }
 public class Praktikum1 {
 
@@ -19,11 +21,19 @@ Pelapor();
 "520414501120";
  bambang.laporan = 
 "gaji tidak sesuai";
+ bambang.nohp = 
+"087456688006";
+ bambang.umur = 
+"20";
  String a = bambang.nama;
  String b = bambang.nik;
  String c = bambang.laporan;
+ String d = bambang.nohp;
+ String e = bambang.umur;
  System.out.println(a);
  System.out.println(b);
  System.out.println(c);
+ System.out.println(d);
+ System.out.println(e);
     }
 }
