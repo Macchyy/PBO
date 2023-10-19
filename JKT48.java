6aea55174fecdf784b133c4f9f4346d6d44ad03a
LaluSirdi <148302969+LaluSirdi@users.noreply.github.com>
2023-10-19 12:22:19 +0800

    Add files via upload

---
+++ JKT48.java
@@ -0,0 +1,33 @@
+package jkt48;
+
+class idol {
+ String nama;
+ String born;
+ String goldar;
+ String zdk;
+ String tinggi;
+ 
+}
+
+public class JKT48 {
+    public static void main(String[] args) {
+         
+        idol flora = new idol(); 
+        idol manda = new idol();
+        
+       flora.nama = "Flora Shafiqa";
+       flora.born = "4 April 2005";
+       flora.goldar = "B";
+       flora.zdk = "Aries";
+       flora.tinggi = "149cm";
+       
+       manda.nama = "Amanda Sukma";
+       manda.born = "17 Desember 2004";
+       manda.goldar = "O";
+       manda.zdk = "Sagitarius";
+       manda.tinggi = "160cm";
+       
+      
+    }
+    
+}
