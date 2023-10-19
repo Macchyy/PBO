3cc122cd58303e3d1165c5d32b1bf49e7e319076
RaditiaAstarina <148021980+RaditiaAstarina@users.noreply.github.com>
2023-10-19 23:18:44 +0800

    Create JB4_F1B019115.java

---
+++ JB4_F1B019115.java
@@ -0,0 +1,27 @@
+package praktikum1;
+import java.util.Scanner;
+
+class Praktikum1 {
+public static void main(String[] args) {
+   Scanner A = new Scanner(System.in);
+   
+   System.out.println("=======pelapor di disnakertrans NTB=======");
+   
+   System.out.print("Nama               : ");
+   String nama = A.nextLine();
+   
+   System.out.print("NoHp               : ");
+   String nohp = A.nextLine(); 
+
+   System.out.print("Laporan            : ");
+   String laporan = A.nextLine(); 
+   
+   System.out.println("----------------------------------- ");
+
+  
+   A.close();
+
+  
+}
+
+}
